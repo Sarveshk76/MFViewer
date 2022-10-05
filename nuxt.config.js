ab35ff33c01c95d9748abbf7c15be297c229958c
Sarveshk76 <kulkarnisarvesh1@gmail.com>
2022-10-05 18:42:08 +0530

    added router to config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,9 @@ export default {
 
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
-
+  router: {
+    base: '/MFViewer/'
+  },
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
